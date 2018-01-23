e44ff10facf27b6c90573b3a961c06fa86c9b359
deminty <31461024+deminty@users.noreply.github.com>
2018-01-23 15:37:16 -0600

    streamlined cell rendering

--- src/components/Cell/index.jsx
+++ src/components/Cell/index.jsx
@@ -31,40 +31,12 @@ export default class Cell extends Component {
     if (this.props.flagged) {
       return (<div className={styles['flagged']} onContextMenu={this.rightClickHandler} />);
     } else if (this.props.component > 0 && this.props.component <= 8) {
-      return (<div className={styles['comp1']} onClick={this.clickHandler} onContextMenu={this.rightClickHandler} />);
+      const s = `comp${this.props.component}`;
+      return (<div className={styles[s]} onClick={this.clickHandler} onContextMenu={this.rightClickHandler} />);
     } else if (this.props.hidden) {
       return (<div className={styles['hidden']} onClick={this.clickHandler} onContextMenu={this.rightClickHandler} />);
     }
-    switch (this.props.mines) {
-    case -1:
-      return (<div className={styles['hasMine']} />);
-
-    case 1:
-      return (<div className={styles['one']} />);
-
-    case 2:
-      return (<div className={styles['two']} />);
-
-    case 3:
-      return (<div className={styles['three']} />);
-
-    case 4:
-      return (<div className={styles['four']} />);
-
-    case 5:
-      return (<div className={styles['five']} />);
-
-    case 6:
-      return (<div className={styles['six']} />);
-
-    case 7:
-      return (<div className={styles['seven']} />);
-
-    case 8:
-      return (<div className={styles['eight']} />);
-
-    default:
-      return (<div className={styles['cell']} />);
-    }
+    const s = `mines${this.props.mines}`;
+    return (<div className={styles[s]} />);
   }
 }
