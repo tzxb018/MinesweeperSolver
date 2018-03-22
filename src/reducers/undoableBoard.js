66d57925d199c19ced0375b0f06ca5c306241864
deminty <31461024+deminty@users.noreply.github.com>
2018-03-22 12:06:03 -0500

    fixed the undo history to exclude changes in the smile

--- src/reducers/undoableBoard.js
+++ src/reducers/undoableBoard.js
@@ -3,14 +3,12 @@ import undoable, { excludeAction } from 'redux-undo';
 import {
   CHANGE_SIZE,
   RESET_BOARD,
-  REVEAL_CELL,
+  CHANGE_SMILE,
  } from 'actions/boardActions';
 
 import board from './board';
 
-const undoableBoard = undoable(board, {
-  filter: excludeAction(REVEAL_CELL),
+export default undoable(board, {
+  filter: excludeAction(CHANGE_SMILE),
   initTypes: [CHANGE_SIZE, RESET_BOARD],
 });
-
-export default undoableBoard;
