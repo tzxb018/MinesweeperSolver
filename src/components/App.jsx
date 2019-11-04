75bcd151d6e41902e8e276cc85bcf380075eee46
Tomo Bessho <tbessho@tbesshoXPS13.unl.edu>
2019-11-04 12:16:24 -0600

    moved the gameboard and history tab under

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import ControlPanel from './control_panel/ControlPanel';
 import HistoryPanel from './history_panel/HistoryPanel';
 import Gameboard from './gameboard/containers/GameboardContainer';
 import SolvePanel from './solve_panel/SolvePanel';
-import ReportError from './control_panel/ReportError';
+// import ReportError from './control_panel/ReportError';
 
 import styles from './style';
 
@@ -12,13 +12,20 @@ export default class App extends Component {
   render() {
     return (
       <div className={styles['container']}>
-        <ControlPanel />
-        <SolvePanel />
-        <div className={styles['gap_grow']} />
-        <Gameboard />
-        <div className={styles['gap']} />
-        <HistoryPanel />
-        <ReportError />
+        <div className={styles['row']}>
+          <ControlPanel />
+          <SolvePanel />
+        </div>
+        <div className={styles['row']}>
+          <HistoryPanel />
+          <div className={styles['gap']} />
+          <Gameboard />
+        </div>
+
+        {/* <div className={styles['gap_grow']} /> */}
+
+
+        {/* <ReportError /> */}
       </div>
     );
   }
